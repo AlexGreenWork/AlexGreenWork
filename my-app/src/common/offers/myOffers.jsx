af9bf32c4e63f27f7eb1a2d309960933d4382295
dreamermx123 <dzargo1@yandex.ru>
2021-11-23 01:54:38 +0300

    myOffers.jsx > post /myOffers

--- my-app/src/common/offers/myOffers.jsx
+++ my-app/src/common/offers/myOffers.jsx
@@ -1,16 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import {NavLink} from "react-router-dom";
 import s from "./sendOffer/offerForm/offers.module.css"
 import {API_URL} from "../../config";
 
+
+
 function Resp() {
+    
     let xhr = new XMLHttpRequest();
-    xhr.open('GET', `${API_URL}api/offers/allOffers`, false)
+
+  /*  xhr.open('GET', `${API_URL}api/offers/allOffers`, false)
     xhr.send();
     console.log(xhr.response)
-    return xhr.response;
+    return xhr.response;*/
+    let userPhoneNumber = localStorage.getItem('userPhoneNumber');
+    let userTabelNum = localStorage.getItem('userTabelNum');
+    let userName = localStorage.getItem('userName');
+    let userSurName = localStorage.getItem('userSurName');
+    let userMiddleName = localStorage.getItem('userMiddleName');
+    
+    let userEmail = localStorage.getItem('userEmail');
+   
 
+    xhr.open('POST', `${API_URL}api/offers/myOffers`, false); /// СИНХРОННЫЙ ЗАПРОС!!!
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send(`firstName=${userName}&userSurName=${userSurName}&middleName=${userMiddleName}&email=${userEmail}`+
+            `&tabelNumber=${userTabelNum}&phoneNumber=${userPhoneNumber}`);
+                 
+           let v = xhr.onreadystatechange;
+       v = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                let result = this.responseText;
+               
+                  console.log("result = " + result);
+                   
+                    // checkRecData(result);
+                    
+                return result
+      }
 
+    }
+    console.log(v)
+    return xhr.response
 }
 
 const Offer = (props) => {
@@ -37,22 +68,22 @@ const Offer = (props) => {
 
 function Message(id) {
     let offersData = JSON.parse(Resp());
-    // console.log(offersData[0]);
+     console.log(offersData[0]);
     offersData.map((offerArr) => {
         if (offerArr.Id === id) {
-            console.log(offerArr);
+            ///console.log(offerArr);
 
             let msg = document.createElement('div');
             msg.className = "message";
             msg.innerHTML = `<label>Имя: ${offerArr.nameSendler}</label>
-    <label>Фамилия: ${offerArr.surnameSendler}</label>
-    <label>Отчество: ${offerArr.middlenameSendler}</label>
-    <label>Номер телефона: ${offerArr.nameSendler}</label>
-    <label>Электронная почта: ${offerArr.email}</label>
-    <label>Дата подачи предложения: ${offerArr.date.slice(0, 10)}</label>
-    <label>Название предложения: ${offerArr.nameOffer}</label>
-    <label>Сдержание предложения: ${offerArr.textOffer}</label>
-    <label>Описание проблемы: ${offerArr.descriptionProblem}</label>`
+                <label>Фамилия: ${offerArr.surnameSendler}</label>
+                <label>Отчество: ${offerArr.middlenameSendler}</label>
+                <label>Номер телефона: ${offerArr.nameSendler}</label>
+                <label>Электронная почта: ${offerArr.email}</label>
+                <label>Дата подачи предложения: ${offerArr.date.slice(0, 10)}</label>
+                <label>Название предложения: ${offerArr.nameOffer}</label>
+                <label>Сдержание предложения: ${offerArr.textOffer}</label>
+                <label>Описание проблемы: ${offerArr.descriptionProblem}</label>`
             document.body.appendChild(msg)
         }
     })
@@ -77,75 +108,3 @@ const Offers = () => {
     )
 }
 export default Offers;
-
-//
-//
-// const Offer = (props) => {
-//     return (
-//         <div className={s.header}>
-//             <div className={s.offerPreview}>
-//                 <div className={s.from}>
-//                     <div className={s.date}>{props.date}</div>
-//                     <div className={s.fromName}>  {props.name}</div>
-//                     <div className={s.status}> : {props.status}</div>
-//
-//                 </div>
-//                 <div className={s.offerText}>{props.nameOffer}
-//                 </div>
-//             </div>
-//         </div>
-//     )
-// }
-//
-// const MyOffers = () => {
-//     let offersData = [
-//         {
-//             date: '12/09/21',
-//             name: 'Ваше предложение',
-//             status: 'Отклонено первоначальной рабочей группой',
-//             nameOffer: 'Изменение мршрута деталей прессового цеха.'
-//         },
-//         {
-//             date: '12/09/21',
-//             name: 'Адрей Валерьевич Крутько',
-//             status: 'Отклонено первоначальной рабочей группой',
-//             nameOffer: 'Сокращение трудового дня.'
-//         },
-//         {
-//             date: '12/09/21',
-//             name: 'Василий Петрович Бидонский',
-//             status: 'Отклонено рабочей группой после рассмотрения подразделением(ями)',
-//             nameOffer: 'Модернизация линии по производству опор'
-//         },
-//         {
-//             date: '12/09/21',
-//             name: 'Сергей Витальевич Слуцкий ',
-//             status: 'Направлено секретарю комиссии',
-//             nameOffer: 'Изменение штатного расписания'
-//         },
-//         {
-//             date: '12/09/21',
-//             name: 'Виктор Петрович Калюжный',
-//             status: 'Результаты рассмотрения комиссией оформлены',
-//             nameOffer: 'Вход по электронным пропускам'
-//         },
-//         {
-//             date: '12/09/21',
-//             name: 'Ирина Викторовна Волкова',
-//             status: 'Направлено для внедрения',
-//             nameOffer: 'Изменение маршрута деталей прессового цеха'
-//         },
-//
-//     ]
-//     return (
-//         <div className={s.offersContainer}>
-//             <div className={s.titleHeader}><NavLink to="/offers"> Предложения</NavLink></div>
-//
-//             <NavLink to='/cardOffer'><Offer name={offersData[0].name} status={offersData[0].status}
-//                                            nameOffer={offersData[0].nameOffer} date={offersData[0].date}/></NavLink>
-//
-//
-//         </div>
-//     )
-// }
-// export default MyOffers;
