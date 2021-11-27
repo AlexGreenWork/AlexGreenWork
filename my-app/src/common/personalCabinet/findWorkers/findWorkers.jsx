fffa9d9a35df0061c58f9599109d671b46eeaa30
Dordovel <sernfst58@mail.ru>
2021-11-27 14:01:52 +0300

    delete debug info and added tbody tab to table

--- my-app/src/common/personalCabinet/findWorkers/findWorkers.jsx
+++ my-app/src/common/personalCabinet/findWorkers/findWorkers.jsx
@@ -4,11 +4,6 @@ import List from "./list"
 import React from "react";
 
 const FindWorkers = () => {
-
-	React.Children.forEach((children, element) => {
-			console.log(element.props);
-	})
-
 	return (
 				<div className={style.sendOfferContainer}>
 					<div className={style.sendOfferInnerContainer}>
