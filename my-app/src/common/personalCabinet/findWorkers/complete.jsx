074b1b77192a9f493467f54041c67fcd0a9e9e9f
Dordovel <sernfst58@mail.ru>
2021-11-27 08:50:09 +0300

    refactor complete module and added cart module structure

--- my-app/src/common/personalCabinet/findWorkers/complete.jsx
+++ my-app/src/common/personalCabinet/findWorkers/complete.jsx
@@ -1,114 +1,116 @@
 import React, {useState} from "react";
 import {AutoComplete, Input} from "antd";
 const axios = require("axios");
+const Cart = require("./cart");
 
-function category_server()
+class Complete extends React.Component
 {
-    return new Map([
-        ["1", "Табельный номер"],
-        ["2", "ФИО"],
-        ["3", "Код цеха"]
-    ]);
-}
+	constructor(props)
+	{
+		super(props);
+		this.state = {options: [],
+						search: []};
 
-function category_server_converter(category)
-{
-    return category_server().get(category);
-}
+		this.search_result_category = new Map([
+												["1", "Табельный номер"],
+												["2", "ФИО"],
+												["3", "Код цеха"]
+											]);
+		this.search_value = this.search_value.bind(this);
+	}
 
-function Header(search, item_category, item_count)
-{
-    const category = ` в ${category_server_converter(item_category)}`;
+	category_server_converter(category)
+	{
+		return this.search_result_category.get(category);
+	}
 
-    return {
-        value: category, label: (<div
-            style={{
-                display: 'flex', justifyContent: 'space-between',
-            }}
-        >
-			<span>
-			  Found `{search}` on {' '}
-                <a
-                    href={`http://localhost:3000/personalCabinet/findWorkers=${item_category}`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-				{category}
-			  </a>
-			</span>
-            <span>{item_count} результатов </span>
-        </div>),
-    };
-}
-
-function Item(value, division, department)
-{
-    return {
-        label:(<div style={{
-            display: 'flex',
-            justifyContent: 'space-between',
-        }}
-        >
-			<span>
-				{value}
-				Отдел {department}
-                Цех {division}
-            </span>
-        </div>),
-    }
-}
-
-function Items(values)
-{
-    let items = [];
-    console.log(values)
-    for(const value of values)
-    {
-        items.push(Item(value.name, value.department, value.division))
-    }
-    return items;
-}
-
-function CreateOptions(search, response)
-{
-    let result = [];
-    for(const item of response.data)
-    {
-        result.push(Header(search, item.category, item.count));
-        result = result.concat(Items(item.users));
-    }
-    return result;
-}
-
-const Complete = () => {
-    const [ options, set_options ] = useState([]);
-    const [ search, set_search ] = useState([]);
-
-    React.useEffect(() => {
-        axios.post("http://localhost:5000/api/user/search", {value: search}).then((res) => {
-            set_options(CreateOptions(search, res));
+	search_value(value)
+	{
+        axios.post("http://localhost:5000/api/user/search", {search: value}).then((res) => {
+            this.setState({options: this.create_options(value, res)});
         })
-    }, [search]);
+	}
 
-    const select = (value) => {
-        console.log('onSelect', value);
-    };
+	create_options(search, response)
+	{
+		let result = [];
+		for(const item of response.data)
+		{
+			result.push(this.create_category_header(search, item.category, item.count));
+			result = result.concat(this.create_items(item.users));
+		}
+		return result;
+	}
 
-    return (<AutoComplete
-        dropdownMatchSelectWidth={500}
-        style={{
-            width: '100%',
-        }}
-        options={options}
-        onSelect={select}
-        onSearch={set_search}
-    >
-        <Input.Search size="large" style={{
-            fontSize: '14px',
-            textAlign: 'center'
-        }
-        } enterButton/>
-    </AutoComplete>);
+	create_items(values)
+	{
+		let items = [];
+		for(const value of values)
+		{
+			items.push(this.create_item(value.name, value.tabnum, value.department, value.division))
+		}
+		return items;
+	}
+
+	create_item(name, tabnum, division, department)
+	{
+		return {
+			label:(<div style={{
+				display: 'flex',
+				justifyContent: 'space-between',
+			}}
+			>
+				<span>
+					{name}
+					Отдел {department}
+					Цех {division}
+				</span>
+			</div>),
+		}
+	}
+
+	create_category_header(search, item_category, item_count)
+	{
+		const category = ` в ${this.category_server_converter(item_category)}`;
+
+		return {
+			value: category, label: (<div
+				style={{
+					display: 'flex', justifyContent: 'space-between',
+				}}
+			>
+				<span>
+				  Found `{search}` on {' '}
+					<a
+						href={`http://localhost:3000/personalCabinet/findWorkers=${item_category}`}
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+					{category}
+				  </a>
+				</span>
+				<span>{item_count} результатов </span>
+			</div>),
+		};
+	}
+
+	render()
+	{
+		return (<AutoComplete
+			dropdownMatchSelectWidth={500}
+			style={{
+				width: '100%',
+			}}
+			options={this.state.options}
+			onSearch={this.search_value}
+		>
+			<Input.Search size="large" style={{
+				fontSize: '14px',
+				textAlign: 'center'
+			}
+			} enterButton/>
+		</AutoComplete>);
+	}
 };
 
 export default Complete
