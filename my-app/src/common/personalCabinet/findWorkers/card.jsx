e1a782e0b945fc0703af9cb130deeb6a7009d381
Dordovel <sernfst58@mail.ru>
2021-12-06 11:01:47 +0300

    fix card component

--- my-app/src/common/personalCabinet/findWorkers/card.jsx
+++ my-app/src/common/personalCabinet/findWorkers/card.jsx
@@ -18,72 +18,61 @@ const Cart = (props) =>
 		}
 	});
 
-	if(info === null
-		|| Object.keys(info).length === 0)
-	{
-		return (
-					<div>
-					</div>
-				);
-	}
-	else
-	{
-		return (
-					<div className = {style.card}>
-						<table>
-							<tbody>
-								<tr>
-									<td>
-										Табельный номер
-									</td>
-									<td>
-										{info.tabnum}
-									</td>
+	return (
+				<div className = {style.card}>
+					<table>
+						<tbody>
+							<tr>
+								<td>
+									Табельный номер
+								</td>
+								<td>
+									{info?.tabnum}
+								</td>
 
-								</tr>
-								<tr>
-									<td>
-										ФИО
-									</td>
-									<td colSpan = "2">
-										{info.name}
-									</td>
-								</tr>
-								<tr>
-									<td>
-										Должность
-									</td>
-									<td colSpan = "2">
-										{info.prof}
-									</td>
-								</tr>
-								<tr>
-									<td>
-										Цех
-									</td>
-									<td colSpan = "2">
-										{info.department}
-									</td>
-								</tr>
-								<tr>
-									<td>
-										Отдел
-									</td>
-									<td colSpan = "2">
-										{info.division}
-									</td>
-								</tr>
-								{/*<tr>*/}
-								{/*	<td>*/}
-								{/*		<button onClick={() => {dispatch(searchtabnum(`${info.tabnum}` ))}}*/}
-								{/*				className = {style.card_button}>Добавить в задачу</button>*/}
-								{/*	</td>*/}
-								{/*</tr>*/}
-							</tbody>
-						</table>
-					</div>
-			);
-    }
+							</tr>
+							<tr>
+								<td>
+									ФИО
+								</td>
+								<td colSpan = "2">
+									{info?.name}
+								</td>
+							</tr>
+							<tr>
+								<td>
+									Должность
+								</td>
+								<td colSpan = "2">
+									{info?.prof}
+								</td>
+							</tr>
+							<tr>
+								<td>
+									Цех
+								</td>
+								<td colSpan = "2">
+									{info?.department}
+								</td>
+							</tr>
+							<tr>
+								<td>
+									Отдел
+								</td>
+								<td colSpan = "2">
+									{info?.division}
+								</td>
+							</tr>
+							{/*<tr>*/}
+							{/*	<td>*/}
+							{/*		<button onClick={() => {dispatch(searchtabnum(`${info.tabnum}` ))}}*/}
+							{/*				className = {style.card_button}>Добавить в задачу</button>*/}
+							{/*	</td>*/}
+							{/*</tr>*/}
+						</tbody>
+					</table>
+				</div>
+		);
 }
 
 export default Cart
