b22b28efa28d7d0f33faefc1007d5c85c815208f
dreamermx123 <dzargo1@yandex.ru>
2021-12-13 10:25:09 +0300

    async Response My Offers

--- my-app/src/common/offers/myOffers.jsx
+++ my-app/src/common/offers/myOffers.jsx
@@ -5,43 +5,16 @@ import {API_URL} from "../../config";
 import {useContext} from "react";
 import Context from "../context/Context";
 
+let requestResult;
+//contextFunction
 
-function Resp() {
-
-    let xhr = new XMLHttpRequest();
-
-    let userPhoneNumber = localStorage.getItem('userPhoneNumber');
-    let userTabelNum = localStorage.getItem('userTabelNum');
-    let userName = localStorage.getItem('userName');
-    let userSurName = localStorage.getItem('userSurName');
-    let userMiddleName = localStorage.getItem('userMiddleName');
-    let userEmail = localStorage.getItem('userEmail');
-
-
-    xhr.open('POST', `${API_URL}api/offers/myOffers`, false); /// СИНХРОННЫЙ ЗАПРОС!!!
-    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhr.send(`firstName=${userName}&userSurName=${userSurName}&middleName=${userMiddleName}&email=${userEmail}` +
-        `&tabelNumber=${userTabelNum}&phoneNumber=${userPhoneNumber}&phoneNumber=${userPhoneNumber}`);
-
-
-    xhr.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            let result = this.responseText;
-
-            return result
-        }
-
-    }
-
-    return xhr.response
-}
 
 const Offer = (props) => {
 
     const value = useContext(Context);
     const [dateComission, setDateComission] = useState('');
 function clickOnOfferLink(){
-
+  
     value.contextFunction(props.id, props.tabelNum)
     localStorage.setItem('idOffers', props.id);
     localStorage.setItem('dateComission', props.dateComission);
@@ -71,9 +44,10 @@ function clickOnOfferLink(){
 
 
 
-const OffersLink = () => {
-    let offersData = JSON.parse(Resp());
+const OffersLink = (props) => {
+    let offersData = JSON.parse(props.request);
    
+       
     return offersData.map((number) =>
         <Offer id={number.Id} date={number.date} name={number.nameSendler}
                surname={number.surnameSendler} midlename={number.middlenameSendler}
@@ -82,10 +56,44 @@ const OffersLink = () => {
 }
 
 const Offers = () => {
+    let portotypeResponse= `[{"Id":"","nameOffer":"","date":"","tabelNum":"","nameSendler":"","surnameSendler":"","middlenameSendler":"","email":"","status":null,"descriptionProblem":"","responsible1":null,"markRespons1":null,"responsible2":null,"markRespons2":null,"responsible3":null,"markRespons3":null,"answerRG":null,"answerTEO":null,"textOffer":"","phoneNumber":" 3","note":null}]`;
+    const [reqMyOff, setReqMyOff] = useState(portotypeResponse);
+
+    function Resp() {
+
+        let xhr = new XMLHttpRequest();
+    
+        let userPhoneNumber = localStorage.getItem('userPhoneNumber');
+        let userTabelNum = localStorage.getItem('userTabelNum');
+        let userName = localStorage.getItem('userName');
+        let userSurName = localStorage.getItem('userSurName');
+        let userMiddleName = localStorage.getItem('userMiddleName');
+        let userEmail = localStorage.getItem('userEmail');
+    
+    
+        xhr.open('POST', `${API_URL}api/offers/myOffers`, true); /// СИНХРОННЫЙ ЗАПРОС!!!
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        
+           
+        xhr.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+               
+               setReqMyOff(xhr.response)
+                          
+            }
+    
+        }
+    
+        xhr.send(`firstName=${userName}&userSurName=${userSurName}&middleName=${userMiddleName}&email=${userEmail}` +
+            `&tabelNumber=${userTabelNum}&phoneNumber=${userPhoneNumber}&phoneNumber=${userPhoneNumber}`);
+    
+        return xhr.response
+    }
+    Resp()
     return (
         <div className={s.offersContainer}>
             <div className={s.titleHeader}>Мои предложения</div>
-            <OffersLink/>
+            <OffersLink request={reqMyOff}/>
 
         </div>
     )
