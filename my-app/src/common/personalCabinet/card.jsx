6b33e2f655ec65f86e732bd0492ca01abe2407ba
Dordovel <sernfst58@mail.ru>
2021-12-14 13:09:41 +0300

    fix components

---
+++ my-app/src/common/personalCabinet/card.jsx
@@ -0,0 +1,141 @@
+import React from "react"
+import {connect} from "react-redux"
+import style from "./card.module.css"
+import {selectcard} from "../../../actions/search";
+
+import {API_URL} from "../../../config.js"
+const {post} = require("axios");
+
+class Cart extends React.Component
+{
+	constructor(props)
+	{
+		super(props);
+		this.state = {info: null};
+		this.componentDidUpdate = this.componentDidUpdate.bind(this);
+		this.update = this.update.bind(this);
+
+		this.update(props);
+	}
+
+	componentDidUpdate(props)
+	{
+		this.update(this.props);
+	}
+
+	update(props)
+	{
+	    if(props?.info
+			&& (
+					this.state.info === null
+					|| this.state.info.tabnum !== props.info
+				) 
+			)
+		{
+			post(`${API_URL}api/user/info`, {search: props.info}).then((res) => {
+				this.setState({info: res.data});
+			})
+		}
+	}
+
+	render()
+	{
+		if(this.state.info === null) return (<></>);
+
+		return (
+					<div className = {style.card} onClick = {() => this.props.dispatch(selectcard(`${this.state.info?.tabnum}`))}>
+						<table>
+							<col width = "20%"/>
+							<col width = "20%"/>
+							<tbody>
+								<tr>
+									<td rowSpan = "8">
+										<div style={
+														{
+															width: "150px",
+															height: "200px", 
+															backgroundImage: `url(${API_URL}files/photos/${this.state.info?.tabnum}.jpg)`,
+															backgroundRepeat: "round",
+															borderRadius: "10px"
+														}
+													}>
+										</div>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										Табельный номер
+									</td>
+									<td>
+										{this.state.info?.tabnum}
+									</td>
+
+								</tr>
+								<tr>
+									<td>
+										ФИО
+									</td>
+									<td colSpan = "2">
+										{this.state.info?.name}
+									</td>
+								</tr>
+								<tr>
+									<td>
+										Должность
+									</td>
+									<td colSpan = "2">
+										{this.state.info?.prof}
+									</td>
+								</tr>
+								<tr>
+									<td>
+										Цех
+									</td>
+									<td colSpan = "2">
+										{this.state.info?.department}
+									</td>
+								</tr>
+								<tr>
+									<td>
+										Отдел
+									</td>
+									<td colSpan = "2">
+										{this.state.info?.division}
+									</td>
+								</tr>
+								{this.state.info?.brigada ?
+									<tr>
+										<td>
+											Бригада
+										</td>
+										<td colSpan = "2">
+											{this.state.info?.brigada}
+										</td>
+									</tr>
+									: null
+								}
+								{this.state.info?.email ?
+									<tr>
+										<td>
+											Электронная Почта
+										</td>
+										<td colSpan = "2">
+											<a href = {`mailto:${this.state.info?.email}`}>{this.state.info?.email}</a>
+										</td>
+									</tr>
+									: null
+								}
+								{/*<tr>*/}
+								{/*	<td>*/}
+								{/*		<button onClick={() => {dispatch(searchtabnum(`${this.state.info.tabnum}` ))}}*/}
+								{/*				className = {style.card_button}>Добавить в задачу</button>*/}
+								{/*	</td>*/}
+								{/*</tr>*/}
+							</tbody>
+						</table>
+					</div>
+			);
+	}
+}
+
+export default connect()(Cart)
---
+++ my-app/src/common/personalCabinet/list.jsx
@@ -0,0 +1,151 @@
+import React from "react"
+import Card from "./card";
+import ScrollButton from "./scroll.jsx"
+import {API_URL} from "../../../config.js"
+import style from "./list.module.css"
+const {post} = require("axios");
+
+class List extends React.Component
+{
+	constructor(props)
+	{
+		super(props);
+
+		this.sort_binds = {
+						name: {
+								false: (lv, rv) => {return lv.name > rv.name},
+								true: (lv, rv) => {return lv.name < rv.name}
+						},
+
+						tab: {
+								false: (lv, rv) => {return lv.tabnum > rv.tabnum},
+								true: (lv, rv) => {return lv.tabnum < rv.tabnum}
+						},
+
+						ceh: {
+								false: (lv, rv) => {return lv.department > rv.department},
+								true: (lv, rv) => {return lv.department < rv.department }
+						},
+
+						dep: {
+								false: (lv, rv) => {return lv.division > rv.division },
+								true: (lv, rv) => {return lv.division < rv.division }
+						}
+		};
+
+		this.state = {
+						values: [],
+						sort: "name",
+						sort_type: false,
+						old_sort: "name"
+					}
+
+		this.search_result_category = new Map([
+												["Табельный номер", "1"],
+												["ФИО", "2"],
+												["Код цеха", "3"],
+												["Название цеха", "4"]
+											]);
+
+		this.create_list = this.create_list.bind(this);
+		this.componentDidUpdate = this.componentDidUpdate.bind(this);
+
+		this.load(this.props.category, this.props.search);
+	}
+
+	sort_list(list)
+	{
+		list.sort(
+					this.sort_binds[this.state.sort]
+									[this.state.sort_type]
+					);
+		return list;
+	}
+
+	create_list(res)
+	{
+		let results = [];
+		if(res?.data)
+		{
+			for(let response of res.data)
+			{
+				if(!response?.users) continue;
+
+				const list = this.sort_list(response.users);
+
+				list.map((v, i) => {
+					results.push(<tr key = {i}>
+									<td colSpan = "4">
+										<Card info = {v.tabnum} />
+									</td>
+								</tr> );
+				});
+			}
+		}
+		
+		return results;
+	}
+
+	load(category, search)
+	{
+		const search_category = this.search_result_category.get(category);
+		post(`${API_URL}api/user/show_category`,
+					{
+						search: search,
+						category: search_category
+					}).then((res) => {
+			this.setState({values: res});
+		})
+	}
+
+	componentDidUpdate(prop)
+	{
+		if(this.props === prop) return;
+		this.load(this.props.category, this.props.search);
+	}
+
+	sort_handle(type)
+	{
+		let new_type = !this.state.sort_type;
+
+		if(this.state.sort != this.state.old_sort)
+		{
+			new_type = false;
+		}
+
+		this.setState({sort: type, sort_type: new_type, old_sort: this.state.sort});
+	}
+
+    render() {
+        return (<div className = {style.listcontainer}>
+					<table className = {style.listtable} cellPadding="12">
+							<col width = "20%"/>
+							<col width = "20%"/>
+							<col width = "20%"/>
+						<thead>
+							<tr>
+								<th onClick={() => {this.sort_handle("tab")}}>
+									Табельный номер
+								</th>
+								<th onClick={() => {this.sort_handle("name")}}>
+									ФИО
+								</th>
+								<th onClick={() => {this.sort_handle("ceh")}}>
+									Цех
+								</th>
+								<th onClick={() => {this.sort_handle("dep")}}>
+									Отдел
+								</th>
+							</tr>
+						</thead>
+						<tbody>
+							{this.create_list(this.state.values)}
+						</tbody>
+					</table>
+					<ScrollButton/>
+				</div>
+        )
+    }
+}
+
+export default List
