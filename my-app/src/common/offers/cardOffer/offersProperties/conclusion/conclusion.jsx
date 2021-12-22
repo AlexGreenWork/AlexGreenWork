235c19050832e8c854ea0332de3c59f72675bf12
alexGreenWork <kpu7k1984@yandex.ru>
2021-12-22 09:48:45 +0300

    update annotation

--- my-app/src/common/offers/cardOffer/offersProperties/conclusion/conclusion.jsx
+++ my-app/src/common/offers/cardOffer/offersProperties/conclusion/conclusion.jsx
@@ -11,6 +11,7 @@ import {useDispatch} from "react-redux";
 import {render} from "react-dom";
 import ConclusionList from "./conclusionList";
 import ViewFileDoc from "../../../../../Pics/svg/ViewFiles/docFileSvg";
+import {element} from "prop-types";
 
 
 console.log(store.getState().search.searchUser)
@@ -124,9 +125,18 @@ const ConclusionOffer = () => {
 
         )
     }
+    const [annotationInp, setAnnotationInp] = React.useState('')
+    function AnnotationInput(e){
+        setAnnotationInp(e.target.value)
+        console.log(e.target.value)
+    }
+
+    function saveRespRGAnnotation(){
+console.log(annotationInp)
+    }
     function IsAdminRGUploadAnnotation() {
         return (<div className={s.fileUpload}>
-            <Button style={{
+            <Button onClick={saveRespRGAnnotation} style={{
                 background:"#e9e9ff",
                 margin:"5px"
             }}>Отправить аннотацию</Button>
@@ -411,7 +421,7 @@ const ConclusionOffer = () => {
                     <div>Краткая аннотация заключения рабочей группы:</div>
                     <div contenteditable="true" className={s.conclusionTextArea} style={{
                         width:"100%"
-                    }}> {store.getState().offers.offer.responsibles_rg?.mark}
+                    }} onInput={AnnotationInput}> {store.getState().offers.offer.responsibles_rg?.mark}
                     </div>
                     <AdminChangeUploadAnnotation isAdmin={localStorage.getItem("userAdminOptions")}/>
                 </div>
