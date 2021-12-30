6c323507bff8baaed8452bbeab3e3ac9201c33e4
dreamermx123 <dzargo1@yandex.ru>
2021-12-30 11:02:21 +0300

    sendler additional

--- my-app/src/common/offers/cardOffer/offersProperties/general/senlerAdditional.jsx
+++ my-app/src/common/offers/cardOffer/offersProperties/general/senlerAdditional.jsx
@@ -17,12 +17,12 @@ function AddSendlerOffers(){
     console.log(objYetSendlers)
         for(let i = 0; i<key.length; i++ ){
 
-        let name = objYetSendlers[key[i]].name
-        let surname = objYetSendlers[key[i]].surname
-        let middlename = objYetSendlers[key[i]].middlename
-     
+        let name = objYetSendlers[key[i]].nameSendler
+        let surname = objYetSendlers[key[i]].surnameSendler
+        let middlename = objYetSendlers[key[i]].middlenameSendler
+       
         elemArr[i] = React.createElement("div", {className:"formFilds",  key:`qw${key[i].toString()}` , value:i }, <label id={`sendler${[i]}`} key={key[i]}> {i+1}: {surname} {name} {middlename}</label> ) ;
-     
+      
         }
 
    } else {
