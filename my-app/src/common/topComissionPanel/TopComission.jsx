84ff288588309dfa0ee45835ef219d19e7b5be14
alexGreenWork <kpu7k1984@yandex.ru>
2022-01-14 13:57:56 +0300

    offer routes notes

--- my-app/src/common/topComissionPanel/TopComission.jsx
+++ my-app/src/common/topComissionPanel/TopComission.jsx
@@ -1,11 +1,23 @@
 import {Budget} from "../../components/dashboard/budget";
+import {LatestOrders} from "../../components/dashboard/latest-orders";
+import {Sales} from "../../components/dashboard/sales";
+
+
 
 const TopComission = () => {
-    return(
-    <Budget style={{
-        gridColumn: "2/3",
-        gridRow: "2/3"
-    }}/>
+    return(<div style={{
+        gridColumn: "2/6",
+            gridRow: "2/6",
+            display: "flex",
+            flexDirection:"column",
+            height: "fit-content",
+            overflowY:"scroll"
+    }}>
+         <Budget />
+
+        <LatestOrders/>
+    </div>
+
     )
 }
 export default TopComission
