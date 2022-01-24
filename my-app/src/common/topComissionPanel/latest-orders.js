41c917126d3c022d8d5865d9a35917b6c51c745c
Dordovel <sernfst58@mail.ru>
2022-01-24 16:19:11 +0300

    refactor history list, fix status detect

--- my-app/src/common/topComissionPanel/latest-orders.js
+++ my-app/src/common/topComissionPanel/latest-orders.js
@@ -1,4 +1,3 @@
-import { v4 as uuid } from 'uuid';
 import PerfectScrollbar from 'react-perfect-scrollbar';
 import {
   Box,
@@ -19,69 +18,6 @@ import React from 'react';
 import server from '../../actions/server';
 import { API_URL } from '../../config';
 import moment from 'moment';
-
-const orders = [
-  {
-    id: uuid(),
-    ref: '231',
-    amount: 30.5,
-    customer: {
-      name: 'Екатерина Танькова'
-    },
-    createdAt: 1555016400000,
-    status: 'в обработке'
-  },
-  {
-    id: uuid(),
-    ref: '250',
-    amount: 25.1,
-    customer: {
-      name: 'Вася Пупкин'
-    },
-    createdAt: 1555016400000,
-    status: 'обработано'
-  },
-  {
-    id: uuid(),
-    ref: '667',
-    amount: 10.99,
-    customer: {
-      name: 'Александр Мордвинкин'
-    },
-    createdAt: 1554930000000,
-    status: 'отклонено'
-  },
-  {
-    id: uuid(),
-    ref: '320',
-    amount: 96.43,
-    customer: {
-      name: 'Анна Кац'
-    },
-    createdAt: 1554757200000,
-    status: 'в обработке'
-  },
-  {
-    id: uuid(),
-    ref: '290',
-    amount: 32.54,
-    customer: {
-      name: 'Кларк Кент'
-    },
-    createdAt: 1554670800000,
-    status: 'обработано'
-  },
-  {
-    id: uuid(),
-    ref: '240',
-    amount: 16.76,
-    customer: {
-      name: 'Адам Денисов'
-    },
-    createdAt: 1554670800000,
-    status: 'обработано'
-  }
-];
 
 class OrdersHeader extends React.Component
 {
@@ -143,11 +79,11 @@ class OrdersList extends React.Component
 	{
 		switch(status)
 		{
-			case 3:
-			case 6:
-			case 11:
+			case '3':
+			case '6':
+			case '11':
 				return 'отклонено'
-			case 13:
+			case '13':
 				return 'обработано'
 			default:
 				return 'в обработке'
@@ -156,7 +92,6 @@ class OrdersList extends React.Component
 
 	render()
 	{
-		console.log(this.state.list)
 		return (
 			<Table>
 				<OrdersHeader/>
