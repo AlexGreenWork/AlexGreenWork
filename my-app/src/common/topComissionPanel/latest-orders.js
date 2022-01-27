329054d084c32f845a83f955d2213abe910e699f
Dordovel <sernfst58@mail.ru>
2022-01-27 12:54:25 +0300

    added sorting to latestOrders component

--- my-app/src/common/topComissionPanel/latest-orders.js
+++ my-app/src/common/topComissionPanel/latest-orders.js
@@ -25,10 +25,29 @@ class OrdersHeader extends React.Component
 	constructor(props)
 	{
 		super(props);
+		this.state = {
+				columns: new Map([['offer_id', {label: 'Номер предложения', sort: 'desc', active: false}],
+									['offer_sendler', {label: 'Автор', sort: 'desc', active: false}],
+									['offer_date', {label: 'Дата', sort: 'desc', active: false}],
+									['offer_status', {label: 'Статус', sort: 'desc', active: false}]
+			])
+		};
+
+		this.onSort = this.onSort.bind(this);
 	}
 
-	sort(id)
+	onSort(event, id)
 	{
+		const column = this.state.columns.get(id);
+		column.sort = column.sort === 'asc'? 'desc': 'asc';
+		column.active = true;
+
+		if(this.props.onSort && typeof this.props.onSort === 'function')
+		{
+			this.props.onSort(id, column.sort);
+		}
+
+		this.setState({columns: this.state.columns});
 	}
 
 	render()
@@ -36,18 +55,16 @@ class OrdersHeader extends React.Component
 		return (
 			<TableHead>
 				<TableRow>
-					<TableCell>
-						Предложение
-					</TableCell>
-					<TableCell>
-						Автор
-					</TableCell>
-					<TableCell >
-						Дата
-					</TableCell>
-					<TableCell>
-						Статус
-					</TableCell>
+					{Array.from(this.state.columns, ([id, column]) => (
+						<TableCell key={id}>
+							<TableSortLabel active={column.active}
+											direction={column.sort}
+											onClick = {(event) => this.onSort(event, id)}
+							>
+								{column.label}
+							</TableSortLabel>
+						</TableCell>
+					))}
 				</TableRow>
 			</TableHead>
 		);
@@ -72,20 +89,51 @@ class OrdersList extends React.Component
 	constructor(props)
 	{
 		super(props);
-		this.state = {page: 0, rowsPerPage: 10};
-		this.handleChangePage = this.handleChangePage.bind(this);
-		this.handleChangeRowsPerPage = this.handleChangeRowsPerPage.bind(this);
+		this.state = {list: this.props.last_offers, page: 0, rowsPerPage: 10};
+		this.handleChangePage = this.onChangePage.bind(this);
+		this.handleChangeRowsPerPage = this.onChangeRowsPerPage.bind(this);
+		this.onSort = this.onSort.bind(this);
 	}
 
-	handleChangePage(event, newPage)
+	componentDidUpdate(props)
+	{
+		if(this.props !== props)
+		{
+			this.setState({list: this.props.last_offers});
+		}
+	}
+
+	onChangePage(event, newPage)
 	{
 		this.setState({page: newPage});
 	};
 
-	handleChangeRowsPerPage(event)
+	onChangeRowsPerPage(event)
 	{
 		this.setState({page: 0, rowsPerPage: parseInt(event.target.value, 10)});
 	};
+
+	sortHandle(lv, rv, type)
+	{
+		if(type === 'asc')
+		{
+			return lv > rv;
+		}
+		else if(type === 'desc')
+		{
+			return lv < rv;
+		}
+	}
+
+	onSort(id, type)
+	{
+		this.state.list.sort((lv, rv) => {
+											return this.sortHandle(lv[id], rv[id], type);
+										}
+		);
+
+		this.setState(this.state.list);
+	}
 
 	render()
 	{
@@ -93,40 +141,40 @@ class OrdersList extends React.Component
 					<TablePagination
 						rowsPerPageOptions={[5, 10, 25, 50]}
 						component="div"
-						count={this.props.last_offers.length}
+						count={this.state.list.length}
 						rowsPerPage={this.state.rowsPerPage}
 						page={this.state.page}
 						labelRowsPerPage = "Отобразить на странице"
-						onPageChange={this.handleChangePage}
-						onRowsPerPageChange={this.handleChangeRowsPerPage}
+						onPageChange={this.onChangePage}
+						onRowsPerPageChange={this.onChangeRowsPerPage}
 						showFirstButton
 						showLastButton
 					/>
 					<TableContainer>
 						<Table>
-							<OrdersHeader/>
+							<OrdersHeader onSort = {this.onSort}/>
 							<TableBody>
-								{this.props.last_offers
+								{this.state.list
 									.slice(this.state.page * this.state.rowsPerPage, this.state.page * this.state.rowsPerPage + this.state.rowsPerPage)
 									.map((order, id) =>
 									(
 										<CustomTableRow key={id} >
 											<TableCell>
-												{order.offer_id}
+												{order['offer_id']}
 											</TableCell>
 											<TableCell>
-												{order.offer_sendler}
+												{order['offer_sendler']}
 											</TableCell>
 											<TableCell>
-												{moment(order.offer_date).format("DD-MM-YYYY")}
+												{moment(order['offer_date']).format("DD-MM-YYYY")}
 											</TableCell>
 											<TableCell>
 												<SeverityPill
-													color={(order.offer_status === 'обработано' && 'success')
-														|| (order.offer_status === 'отклонено' && 'error')
+													color={(order['offer_status'] === 'обработано' && 'success')
+														|| (order['offer_status'] === 'отклонено' && 'error')
 															|| 'warning'}
 												>
-													{order.offer_status}
+													{order['offer_status']}
 												</SeverityPill>
 											</TableCell>
 										</CustomTableRow>
@@ -137,12 +185,12 @@ class OrdersList extends React.Component
 					<TablePagination
 						rowsPerPageOptions={[5, 10, 25, 50]}
 						component="div"
-						count={this.props.last_offers.length}
+						count={this.state.list.length}
 						rowsPerPage={this.state.rowsPerPage}
 						page={this.state.page}
 						labelRowsPerPage = "Отобразить на странице"
-						onPageChange={this.handleChangePage}
-						onRowsPerPageChange={this.handleChangeRowsPerPage}
+						onPageChange={this.onChangePage}
+						onRowsPerPageChange={this.onChangeRowsPerPage}
 						showFirstButton
 						showLastButton
 					/>
