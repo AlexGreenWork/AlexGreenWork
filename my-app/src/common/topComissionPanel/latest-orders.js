0aff53659d0930df9951b7e14f59df1eab4278fd
Dordovel <sernfst58@mail.ru>
2022-01-27 09:45:48 +0300

    added filter checkbox to LatestOrders component

--- my-app/src/common/topComissionPanel/latest-orders.js
+++ my-app/src/common/topComissionPanel/latest-orders.js
@@ -11,7 +11,9 @@ import {
 	TablePagination,
 	Tooltip,
 	Paper,
-	TableContainer
+	TableContainer,
+	Checkbox,
+    FormControlLabel
 } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import { SeverityPill } from './severity-pill';
@@ -149,11 +151,62 @@ class OrdersList extends React.Component
 	}
 }
 
+class CheckBox extends React.Component
+{
+	constructor(props)
+	{
+		super(props)
+		this.state = {check: this.props.check}
+		this.check = this.check.bind(this);
+	}
+
+	check(event)
+	{
+		this.setState({check: event.target.checked})
+		if(this.props.checked && typeof this.props.checked === 'function')
+		{
+			this.props.checked(!this.state.check, this.props.label);
+		}
+	}
+
+	render()
+	{
+		return (
+				<FormControlLabel
+					  control={<Checkbox checked={this.state.check} onChange={this.check} color={this.props.color? this.props.color : `default`}/>}
+					  label={this.props.label}/>
+		);
+	}
+}
+
 class LatestOrders extends React.Component
 {
 	constructor(props)
 	{
 		super(props);
+		this.state = {list: this.props.last_offers,
+						box: new Map([
+							['в обработке', true],
+							['обработано', true],
+							['отклонено', true]
+						])};
+		this.check = this.check.bind(this);
+	}
+
+	componentDidUpdate(props)
+	{
+		if(this.props !== props
+			&& this.props.last_offers.length > 0)
+		{
+			this.setState({list:this.props.last_offers});
+		}
+	}
+
+	check(status, label)
+	{
+		this.state.box.set(label,status);
+		const list = this.props.last_offers.filter((obj) => {return this.state.box.get(obj.offer_status)});
+		this.setState({list: list, box: this.state.box});
 	}
 
 	render()
@@ -162,7 +215,18 @@ class LatestOrders extends React.Component
 		  <Card {...this.props}>
 			<CardHeader title="Последние предложения" />
 			  <Box sx={{ minWidth: 800 }}>
-				<OrdersList last_offers = {this.props.last_offers}/>
+				<div style={{height: "auto", width: "100%", paddingLeft: "16px"}}>
+					{Array.from(this.state.box, ([label, status], id) => <CheckBox key = {id}
+																					label = {label}
+																					checked = {this.check}
+																					check = {status}
+																					color={
+																							(label === 'обработано' && 'success')
+																								|| (label === 'отклонено' && 'error')
+																									|| 'warning'}/>)
+																							}
+				</div>
+				<OrdersList last_offers = {this.state.list}/>
 			  </Box>
 		  </Card>
 		);
