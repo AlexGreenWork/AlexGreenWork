8be09215b1ba3f2c1ef628336ae7a58935864019
Dordovel <sernfst58@mail.ru>
2022-02-28 11:29:20 +0300

    added responsibles info, last offer link to user card

--- my-app/src/common/topComissionPanel/userCard/user_stats.jsx
+++ my-app/src/common/topComissionPanel/userCard/user_stats.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import server from '../../../actions/server';
 import { API_URL } from '../../../config';
 import moment from 'moment';
+import { CardOfferLinkAdapter } from '../../offers/offers';
 
 class State extends React.Component
 {
@@ -10,9 +11,15 @@ class State extends React.Component
 		super(props);
 		this.state = {list: []}
 		this.componentDidMount = this.componentDidMount.bind(this);
+		this.load = this.load.bind(this);
 	}
 
 	componentDidMount()
+	{
+		this.load();
+	}
+
+	load()
 	{
 		server.send_post_request(`${API_URL}api/offers/userOfferStates`, {user: this.props.info})
 			.then((res) => {
@@ -31,6 +38,9 @@ class State extends React.Component
 		this.setState({list: data.self_offers,
 						sum: sum,
 						register: data.user,
+						responsibles: data.responsibles,
+						responsibles_rg: data.responsibles_rg,
+						last_offer_date: {...data.last_offer_date, date: moment(data.last_offer_date.date).format('DD-MM-YYYY') },
 						co_offers: data.co_offers});
 	}
 
@@ -39,12 +49,23 @@ class State extends React.Component
 		return value[0].toUpperCase() + value.substring(1);
 	}
 
+	create_offer_link(data)
+	{
+		const nameOffer = `№ ${data?.Id}: ${this.upper(data?.nameOffer)}`;
+
+		return (<span onMouseEnter={(e) => {e.target.innerText = " Открыть предложение"}}
+			onMouseOut={(e) => {e.target.innerText = ` ${nameOffer}`}}>
+				&nbsp;{nameOffer}
+		</span>)
+	}
+
 	render()
 	{
 		return (<>
 				<div style={{paddingTop: "16px", borderBottom: "2px solid #f0f0f0"}}>
 					<span style={{fontSize: "17px", fontWeight: "bold"}}>Сведения:</span>
 				</div>
+
 				<span style={{fontWeight: "bold", fontSize: "15px"}}>
 					Зарегистрирован(a):
 				</span>
@@ -52,6 +73,7 @@ class State extends React.Component
 				<span>
 					{moment(this.state.register).format("DD-MM-YYYY")}
 				</span>
+
 				<div style={{display: "grid", gridTemplateColumns:"auto auto auto auto", paddingTop: "24px"}}>
 					<div>
 						<span style={{fontWeight: "bold", fontSize: "15px"}}>
@@ -59,8 +81,8 @@ class State extends React.Component
 						</span>
 						&nbsp;{this.state.sum}
 					</div>
-					{this.state.list.map((value) => (
-						<div>
+					{this.state.list.map((value, id) => (
+						<div key={id}>
 							<span style={{fontWeight: "bold", fontSize: "15px"}}>
 								{this.upper(value.info)}:
 							</span>
@@ -72,9 +94,60 @@ class State extends React.Component
 				<div style={{display: "grid", gridTemplateColumns:"auto auto auto auto", paddingTop: "24px"}}>
 					<div>
 						<span style={{fontWeight: "bold", fontSize: "15px"}}>
-							Соавторств:
+							Последнее предложение:
+						</span>
+						{this.state.last_offer_date?.Id ?
+							<CardOfferLinkAdapter
+								  id={this.state.last_offer_date?.Id}
+								  date={this.state.last_offer_date?.date}
+								  name={this.state.last_offer_date?.nameSendler}
+								  surname={this.state.last_offer_date?.surnameSendler}
+								  midlename={this.state.last_offer_date?.middlenameSendler}
+								  status={this.state.last_offer_date?.status}
+								  nameOffer={this.state.last_offer_date?.nameOffer}
+								  tabelNum={this.state.last_offer_date?.tabelNum}
+								  dateComission={this.state.last_offer_date?.dateComission}
+								  email={this.state.last_offer_date?.email}
+							>
+								{this.create_offer_link(this.state.last_offer_date)}
+							</CardOfferLinkAdapter>
+						: null}
+					</div>
+				</div>
+
+				<div style={{display: "grid", gridTemplateColumns:"auto auto auto auto", paddingTop: "24px"}}>
+					<div>
+						<span style={{fontWeight: "bold", fontSize: "15px"}}>
+							Последнее предложение подано:
+						</span>
+						&nbsp;{this.state.last_offer_date?.date}
+					</div>
+				</div>
+
+				<div style={{display: "grid", gridTemplateColumns:"auto auto auto auto", paddingTop: "24px"}}>
+					<div>
+						<span style={{fontWeight: "bold", fontSize: "15px"}}>
+							Был Соавтором:
 						</span>
 						&nbsp;{this.state.co_offers}
+					</div>
+				</div>
+
+				<div style={{display: "grid", gridTemplateColumns:"auto auto auto auto", paddingTop: "24px"}}>
+					<div>
+						<span style={{fontWeight: "bold", fontSize: "15px"}}>
+							Был назначен ответственным сотрудником:
+						</span>
+						&nbsp;{this.state.responsibles}
+					</div>
+				</div>
+
+				<div style={{display: "grid", gridTemplateColumns:"auto auto auto auto", paddingTop: "24px"}}>
+					<div>
+						<span style={{fontWeight: "bold", fontSize: "15px"}}>
+							Был в составе рабочей группы:
+						</span>
+						&nbsp;{this.state.responsibles_rg}
 					</div>
 				</div>
 			</>);
